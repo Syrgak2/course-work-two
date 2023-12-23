befa325b63ca9de40b1a83fa45595c7921a6693b
Syrgak <Kary6ekov.s@gmail.com>
2023-12-23 18:24:21 +0100

    project created

---
+++ src/main/java/com/example/course/work/CourseWorkTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.course.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkTwoApplication.class, args);
+	}
+
+}
